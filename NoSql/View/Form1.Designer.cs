cd134403de4fa512d29c32f09228ec1b712f8052
Gelder, Kimberly van <565459@student.inholland.nl>
2020-03-05 16:03:56 +0100

    test

--- NoSql/View/Form1.Designer.cs
+++ NoSql/View/Form1.Designer.cs
@@ -49,7 +49,9 @@
             this.panelBorder.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panelMenu.SuspendLayout();
-            this.panelIncident.SuspendLayout();
+            this.PanelUser.SuspendLayout();
+            this.PanelIncident.SuspendLayout();
+            this.PanelDash.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelBorder
@@ -59,8 +61,9 @@
             this.panelBorder.Controls.Add(this.pictureBox1);
             this.panelBorder.Controls.Add(this.lblUser);
             this.panelBorder.Location = new System.Drawing.Point(2, 2);
+            this.panelBorder.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelBorder.Name = "panelBorder";
-            this.panelBorder.Size = new System.Drawing.Size(1189, 100);
+            this.panelBorder.Size = new System.Drawing.Size(1057, 80);
             this.panelBorder.TabIndex = 3;
             // 
             // lblTitle
@@ -68,9 +71,9 @@
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Lato Light", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTitle.ForeColor = System.Drawing.Color.White;
-            this.lblTitle.Location = new System.Drawing.Point(201, 27);
+            this.lblTitle.Location = new System.Drawing.Point(179, 22);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(445, 64);
+            this.lblTitle.Size = new System.Drawing.Size(374, 54);
             this.lblTitle.TabIndex = 2;
             this.lblTitle.Text = "The Garden Group";
             // 
@@ -79,9 +82,10 @@
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(9, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(8, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(155, 97);
+            this.pictureBox1.Size = new System.Drawing.Size(138, 78);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -89,9 +93,9 @@
             // 
             this.lblUser.AutoSize = true;
             this.lblUser.ForeColor = System.Drawing.Color.White;
-            this.lblUser.Location = new System.Drawing.Point(1030, 71);
+            this.lblUser.Location = new System.Drawing.Point(916, 57);
             this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(51, 20);
+            this.lblUser.Size = new System.Drawing.Size(46, 17);
             this.lblUser.TabIndex = 0;
             this.lblUser.Text = "label3";
             // 
@@ -102,9 +106,10 @@
             this.panelMenu.Controls.Add(this.btnUser);
             this.panelMenu.Controls.Add(this.btnDash);
             this.panelMenu.Controls.Add(this.btnIncident);
-            this.panelMenu.Location = new System.Drawing.Point(2, 102);
+            this.panelMenu.Location = new System.Drawing.Point(2, 82);
+            this.panelMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelMenu.Name = "panelMenu";
-            this.panelMenu.Size = new System.Drawing.Size(202, 592);
+            this.panelMenu.Size = new System.Drawing.Size(180, 474);
             this.panelMenu.TabIndex = 4;
             // 
             // btnLogOff
@@ -114,9 +119,10 @@
             this.btnLogOff.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogOff.Font = new System.Drawing.Font("Lato Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogOff.ForeColor = System.Drawing.Color.White;
-            this.btnLogOff.Location = new System.Drawing.Point(0, 357);
+            this.btnLogOff.Location = new System.Drawing.Point(0, 286);
+            this.btnLogOff.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogOff.Name = "btnLogOff";
-            this.btnLogOff.Size = new System.Drawing.Size(202, 75);
+            this.btnLogOff.Size = new System.Drawing.Size(180, 60);
             this.btnLogOff.TabIndex = 4;
             this.btnLogOff.Text = "Log Off";
             this.btnLogOff.UseVisualStyleBackColor = false;
@@ -128,9 +134,10 @@
             this.btnUser.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnUser.Font = new System.Drawing.Font("Lato Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnUser.ForeColor = System.Drawing.Color.White;
-            this.btnUser.Location = new System.Drawing.Point(0, 246);
+            this.btnUser.Location = new System.Drawing.Point(0, 197);
+            this.btnUser.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnUser.Name = "btnUser";
-            this.btnUser.Size = new System.Drawing.Size(202, 75);
+            this.btnUser.Size = new System.Drawing.Size(180, 60);
             this.btnUser.TabIndex = 2;
             this.btnUser.Text = "User Mangement";
             this.btnUser.UseVisualStyleBackColor = false;
@@ -144,9 +151,10 @@
             this.btnDash.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDash.Font = new System.Drawing.Font("Lato Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDash.ForeColor = System.Drawing.Color.White;
-            this.btnDash.Location = new System.Drawing.Point(0, 30);
+            this.btnDash.Location = new System.Drawing.Point(0, 24);
+            this.btnDash.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnDash.Name = "btnDash";
-            this.btnDash.Size = new System.Drawing.Size(202, 75);
+            this.btnDash.Size = new System.Drawing.Size(180, 60);
             this.btnDash.TabIndex = 0;
             this.btnDash.Text = "Dashboard";
             this.btnDash.UseVisualStyleBackColor = false;
@@ -159,9 +167,10 @@
             this.btnIncident.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIncident.Font = new System.Drawing.Font("Lato Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIncident.ForeColor = System.Drawing.Color.White;
-            this.btnIncident.Location = new System.Drawing.Point(0, 138);
+            this.btnIncident.Location = new System.Drawing.Point(0, 110);
+            this.btnIncident.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnIncident.Name = "btnIncident";
-            this.btnIncident.Size = new System.Drawing.Size(202, 75);
+            this.btnIncident.Size = new System.Drawing.Size(180, 60);
             this.btnIncident.TabIndex = 1;
             this.btnIncident.Text = "Incident Mangement";
             this.btnIncident.UseVisualStyleBackColor = false;
@@ -169,23 +178,23 @@
             // 
             // panelDash
             // 
-            this.panelDash.Location = new System.Drawing.Point(226, 132);
-            this.panelDash.Name = "panelDash";
-            this.panelDash.Size = new System.Drawing.Size(956, 542);
-            this.panelDash.TabIndex = 5;
+            this.PanelUser.Controls.Add(this.label3);
+            this.PanelUser.Location = new System.Drawing.Point(232, 135);
+            this.PanelUser.Name = "PanelUser";
+            this.PanelUser.Size = new System.Drawing.Size(937, 527);
+            this.PanelUser.TabIndex = 5;
             // 
             // panelUser
             // 
-            this.panelUser.Location = new System.Drawing.Point(226, 132);
-            this.panelUser.Name = "panelUser";
-            this.panelUser.Size = new System.Drawing.Size(956, 542);
-            this.panelUser.TabIndex = 6;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "User";
             // 
             // panelIncident
             // 
-            this.panelIncident.Controls.Add(this.listBox1);
-            this.panelIncident.Controls.Add(this.btnCreateIncident);
-            this.panelIncident.Controls.Add(this.txtFilter);
             this.panelIncident.Controls.Add(this.label1);
             this.panelIncident.Controls.Add(this.lblTitleIndicent);
             this.panelIncident.Location = new System.Drawing.Point(226, 129);
@@ -193,35 +202,15 @@
             this.panelIncident.Size = new System.Drawing.Size(956, 542);
             this.panelIncident.TabIndex = 6;
             // 
-            // listBox1
+            // lblTitleIndicent
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
-            this.listBox1.Location = new System.Drawing.Point(144, 210);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(651, 284);
-            this.listBox1.TabIndex = 4;
-            // 
-            // btnCreateIncident
-            // 
-            this.btnCreateIncident.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(191)))), ((int)(((byte)(49)))));
-            this.btnCreateIncident.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCreateIncident.Font = new System.Drawing.Font("Lato Light", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreateIncident.ForeColor = System.Drawing.Color.White;
-            this.btnCreateIncident.Location = new System.Drawing.Point(600, 124);
-            this.btnCreateIncident.Name = "btnCreateIncident";
-            this.btnCreateIncident.Size = new System.Drawing.Size(195, 48);
-            this.btnCreateIncident.TabIndex = 3;
-            this.btnCreateIncident.Text = "Create Incident";
-            this.btnCreateIncident.UseVisualStyleBackColor = false;
-            // 
-            // txtFilter
-            // 
-            this.txtFilter.Location = new System.Drawing.Point(144, 138);
-            this.txtFilter.Name = "txtFilter";
-            this.txtFilter.Size = new System.Drawing.Size(177, 26);
-            this.txtFilter.TabIndex = 2;
-            this.txtFilter.Text = "Filter by Email";
+            this.lblTitleIndicent.AutoSize = true;
+            this.lblTitleIndicent.Font = new System.Drawing.Font("Lato Light", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitleIndicent.Location = new System.Drawing.Point(271, 24);
+            this.lblTitleIndicent.Name = "lblTitleIndicent";
+            this.lblTitleIndicent.Size = new System.Drawing.Size(410, 54);
+            this.lblTitleIndicent.TabIndex = 0;
+            this.lblTitleIndicent.Text = "Incident Mangement";
             // 
             // label1
             // 
@@ -229,9 +218,35 @@
             this.label1.Font = new System.Drawing.Font("Lato Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(423, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 30);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Tickets";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Incident";
+            // 
+            // PanelDash
+            // 
+            this.PanelDash.Controls.Add(this.label2);
+            this.PanelDash.Location = new System.Drawing.Point(232, 135);
+            this.PanelDash.Name = "PanelDash";
+            this.PanelDash.Size = new System.Drawing.Size(937, 527);
+            this.PanelDash.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dash";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(464, 272);
+            this.dataGridView1.TabIndex = 3;
             // 
             // lblTitleIndicent
             // 
@@ -245,14 +260,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1194, 696);
-            this.Controls.Add(this.panelIncident);
-            this.Controls.Add(this.panelUser);
-            this.Controls.Add(this.panelDash);
+            this.Controls.Add(this.PanelDash);
+            this.Controls.Add(this.PanelUser);
+            this.Controls.Add(this.PanelIncident);
             this.Controls.Add(this.panelMenu);
             this.Controls.Add(this.panelBorder);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -260,8 +276,12 @@
             this.panelBorder.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panelMenu.ResumeLayout(false);
-            this.panelIncident.ResumeLayout(false);
-            this.panelIncident.PerformLayout();
+            this.PanelUser.ResumeLayout(false);
+            this.PanelUser.PerformLayout();
+            this.PanelIncident.ResumeLayout(false);
+            this.PanelIncident.PerformLayout();
+            this.PanelDash.ResumeLayout(false);
+            this.PanelDash.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -282,9 +302,6 @@
         private System.Windows.Forms.Panel panelIncident;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblTitleIndicent;
-        private System.Windows.Forms.TextBox txtFilter;
-        private System.Windows.Forms.Button btnCreateIncident;
-        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
