adde8fa0e763736814671d77651fc1b5017393f0
Steff Burgering <steffburgering@hotmail.com>
2020-04-01 12:19:25 +0200

    changed moduserclass

--- NoSql/Model/ModUser.cs
+++ NoSql/Model/ModUser.cs
@@ -11,11 +11,16 @@ namespace Controller
     public class ModUser
     {
         [BsonId]
-        public ObjectId Id { get; set; }
-        public string Name { get; set; }
+        public ObjectId ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public int Role { get; set; }
+        public double PhoneNr { get; set; }
+        public int Location { get; set; }
+        public int NrOfTickets { get; set; }
+        public int Id { get; set; }
     }
 }
    
